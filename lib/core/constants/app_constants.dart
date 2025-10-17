4a9b9c484924f8d248ef5633bd94bd33f0252144
DivergenteNM <nicolasmdivergent@gmail.com>
2025-10-16 19:21:35 -0500

    feat(constants): add constants

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,35 @@
+class AppConstants {
+  // Categorías
+  static const List<String> categoriasIngreso = [
+    "Ayuda Familiar",
+    "Entradas esporádicas",
+    "Emprendimiento",
+    "Otros ingresos"
+  ];
+
+  static const List<String> categoriasEgreso = [
+    "Alimentación",
+    "Transporte",
+    "Servicios y alojamiento",
+    "Estudios y universidad",
+    "Salud y cuidado personal",
+    "Eventos y vida social",
+    "Tecnología y regalos",
+    "Ahorros y proyectos",
+    "Otros egresos"
+  ];
+
+  // Canales de pago
+  static const List<String> canales = [
+    "Nequi",
+    "NuBank",
+    "Efectivo"
+  ];
+
+  // Tipos de transacción
+  static const List<String> tiposTransaccion = [
+    "Ingreso",
+    "Egreso",
+    "Transferencia"
+  ];
+}
